ba36b5cb155025a69fb64c51394aebc238270c16
paraj <bhattparaj2@gmail.com>
2024-02-28 13:44:08 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Button, HStack } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
+  
 const Header = () => {
   return (
     <HStack p={"4"} shadow={"base"}bgColor={"blackAlpha.900"}>
