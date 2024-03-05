d1f5a5f42bc86835892dd56a908865a5a7eda522
paraj <bhattparaj2@gmail.com>
2024-03-05 14:09:22 +0530

    remove light mode from website

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import ColorModeSwitcher from './ColorModeSwitcher';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme} > 
-    <ColorModeScript />
-    <ColorModeSwitcher/>
+    <ChakraProvider  > 
+    {/* <ColorModeScript /> */}
+    {/* <ColorModeSwitcher/> */}
       <App />
     </ChakraProvider>
   </React.StrictMode>
